80fb0f66e76755ba9868b88e6e4ba301f433010d
Guillermo Ferrer <guilleferrer@gmail.com>
2014-04-15 08:59:40 +0200

    Adapts multiple_choice field to the new usage of multiple-choice directive

--- Form/Type/MultipleChoiceType.php
+++ Form/Type/MultipleChoiceType.php
@@ -2,13 +2,24 @@
 
 namespace Undf\FormBundle\Form\Type;
 
+use JMS\Serializer\Serializer;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
+
 class MultipleChoiceType extends EntityType
 {
+
+    private $serializer;
+
+    public function __construct($doctrine, $propertyAccessor, Serializer $serializer)
+    {
+        parent::__construct($doctrine, $propertyAccessor);
+
+        $this->serializer = $serializer;
+    }
 
     public function getName()
     {
@@ -18,11 +29,27 @@ class MultipleChoiceType extends EntityType
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         parent::setDefaultOptions($resolver);
-        $resolver->setRequired(array('api_url'));
     }
 
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
-        $view->vars['api_url'] = $options['api_url'];
+        $choices = array();
+        foreach ($view->vars['choices'] as $choiceView) {
+
+            $choice = array();
+            $choice['id'] = $choiceView->data->getId();
+            $choice['name'] = $choiceView->data->getName();
+
+            foreach ($form->getData()->toArray() as $selected) {
+                if ($selected->getId() == $choiceView->data->getId()) {
+                    $choice['selected'] = true;
+                    break;
+                }
+            }
+
+            $choices[] = $choice;
+        }
+
+        $view->vars['json_choices'] = $this->serializer->serialize($choices, 'json');
     }
 }
